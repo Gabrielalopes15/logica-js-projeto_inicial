8fa5e540591f687aa005ff17460e336f45cf83cb
Gabrielalopes15 <gabylopes2000@gmail.com>
2025-10-08 18:05:13 -0300

    condicoes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,16 +1,18 @@
 alert ('Boas vindas ao jogo do número secreto!');
 let numeroSecreto = 10;
-console.log(numeroSecreto)
+console.log(numeroSecreto);
+let tentativas = 1;
 let chute = prompt('Escolha um numero entre 1 e 30');
 
-
-if (numeroSecreto == chute){
-    alert(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
-}else {
-    if(numeroSecreto > chute){
-        alert(`O numero secreto é menor que ${chute} !`)
-    }else{
-        alert(`O numero secreto é maior que ${chute} !`)
-    }
+while (chute != numeroSecreto){
+    let chute = prompt('Escolha um numero entre 1 e 30');
+        if (numeroSecreto == chute){
+            alert(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
+        }else {
+            if(chute > numeroSecreto){
+                alert(`O numero secreto é menor que ${chute} !`);
+            }else{
+                alert(`O numero secreto é maior que ${chute} !`);
+            }
+        }
 }
-
