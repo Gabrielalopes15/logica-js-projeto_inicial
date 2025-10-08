0f15624b3fd1340601fc882d8fbbbc5f3e26abf6
Gabrielalopes15 <gabylopes2000@gmail.com>
2025-10-08 17:39:17 -0300

    aula 2

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,1 +1,32 @@
 alert ('Boas vindas ao jogo do número secreto!');
+let numeroSecreto = 10;
+console.log(numeroSecreto)
+let chute = prompt('Escolha um numero entre 1 e 30');
+
+
+if (numeroSecreto == chute){
+    alert(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
+}else {
+    alert('Você errou :(')
+}
+
+/*
+alert('Boas vindas ao nosso site!');
+let = nome = 'Lua';
+let = idade = 25;
+
+let = numeroDeVendas = 50;
+let = saldoDisponivel = 1000;
+
+alert ('Erro!Preencha todos os campos.');
+
+let = mensagemDeErro  = 'Erro! Preencha todos os campos';
+alert(mensagemDeErro);
+
+let = nomeInput = prompt ('Informe seu nome:');
+let = idadeInput = prompt ('Informe sua idade:');
+
+if (idade > 18){
+    alert('Pode tirar habilitação')
+}else('Você ainda não atingiu a maioridade.')
+*/
