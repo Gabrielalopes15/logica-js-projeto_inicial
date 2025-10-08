f7135685e29f5c597993cc4f643960213c6644b4
Gabrielalopes15 <gabylopes2000@gmail.com>
2025-10-08 17:47:17 -0300

    condicoes

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -7,26 +7,10 @@ let chute = prompt('Escolha um numero entre 1 e 30');
 if (numeroSecreto == chute){
     alert(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
 }else {
-    alert('Você errou :(')
+    if(numeroSecreto > chute){
+        alert(`O numero secreto é menor que ${chute} !`)
+    }else{
+        alert(`O numero secreto é maior que ${chute} !`)
+    }
 }
 
-/*
-alert('Boas vindas ao nosso site!');
-let = nome = 'Lua';
-let = idade = 25;
-
-let = numeroDeVendas = 50;
-let = saldoDisponivel = 1000;
-
-alert ('Erro!Preencha todos os campos.');
-
-let = mensagemDeErro  = 'Erro! Preencha todos os campos';
-alert(mensagemDeErro);
-
-let = nomeInput = prompt ('Informe seu nome:');
-let = idadeInput = prompt ('Informe sua idade:');
-
-if (idade > 18){
-    alert('Pode tirar habilitação')
-}else('Você ainda não atingiu a maioridade.')
-*/
