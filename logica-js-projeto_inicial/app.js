30c7e65c08afa0d6da2e40a72262a7087976b04b
Gabrielalopes15 <gabylopes2000@gmail.com>
2025-10-09 08:54:23 -0300

    aula 3

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -7,7 +7,7 @@ let chute = prompt('Escolha um numero entre 1 e 30');
 while (chute != numeroSecreto){
     let chute = prompt('Escolha um numero entre 1 e 30');
         if (numeroSecreto == chute){
-            alert(`Isso ai você descobriu o número secreto ${numeroSecreto} `);
+            alert(`Isso ai você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentivas`);
         }else {
             if(chute > numeroSecreto){
                 alert(`O numero secreto é menor que ${chute} !`);
@@ -15,4 +15,6 @@ while (chute != numeroSecreto){
                 alert(`O numero secreto é maior que ${chute} !`);
             }
         }
+        tentativas++;
+
 }
